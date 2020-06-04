fe4f0340ba13c11f288642869cc0e279b0fdd8a1
HelenaXu <xuxue2291@itu.tsudents.edu>
2020-06-04 15:35:58 -0500

    with mocked API

--- netflix-movie-mock/src/actions/index.js
+++ netflix-movie-mock/src/actions/index.js
@@ -18,16 +18,16 @@ export const fetchData = data => ({
 });
 
 
-// because we do not have the URL for HTTP request, so I use a mocked API with the given data
+// I use a mocked API with the given data
 export const getFirstRender = () => {
   return dispatch => {
     axios.get('https://1acf7de0-7572-4158-b0f2-cceab84e6977.mock.pstmn.io/demo')
-    .then(response =>{
-      const data = response.data
-      dispatch(fetchData(data))
-    })
-    .catch(() => {
-          alert('there is an error when fetching the data!!');
-    });
+      .then(response => {
+        const data = response.data
+        dispatch(fetchData(data))
+      })
+      .catch(() => {
+        alert('there is an error when fetching the data!!');
+      });
   }
 };
